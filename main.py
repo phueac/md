0aa7848e04124a02c531897e2fa0c1166964591f
David Edmunds <david.m.edmunds@gmail.com>
2014-03-23 21:44:07 +0000

    Add (broken) force routine

--- main.py
+++ main.py
@@ -25,6 +25,15 @@ def twelve_six(A, B, r):
     r12 = r6 * r6
     return (A/r12) - (B/r6)
 
+def twelve_six_force(A, B, r):
+    """Returns the Lennard-Jones force"""
+    r2 = r*r 
+    r6 = r2 * r2 * r2
+    r7 = r6 * r
+    r12 = r6 * r6
+    r13 = r12 * r 
+    return -12*A/r13 + 6*B/r7
+
 def pe(x):
     """Returns potential energy of system.
 
@@ -39,11 +48,39 @@ def pe(x):
             sum += twelve_six(A, B, r) 
     return sum
 
+def f(x):
+    """Returns force on each atom.
+
+    Arguments:
+    x -- (natoms x 3) Numpy array of atomic positions
+
+    Returns:
+    force -- (natoms x 3) Numpy array of atomic forces
+
+    """
+    force = np.zeros((natoms, natoms, 3))
+    for i in range(natoms):
+        for j in range(i+1, natoms):
+            s  = np.abs(x[j] - x[i])
+            fx = twelve_six_force(A, B, s[0])
+            fy = twelve_six_force(A, B, s[1])
+            fz = twelve_six_force(A, B, s[2])
+            force[i][j] = [fx, fy, fz]
+    return force
+
+
 def d(x1, x2):
     s = x1 - x2
     return np.sqrt(np.dot(s, s))
 
+def verlet(x):
+    """Integrates the equations of motion and returns new particle
+    positions."""
+    xnew = 2*x - xold + dtsq*a
+
 if __name__ == '__main__':
     carbon = Atom(12.0, 'C')
-    pts = simple_cubic_lattice(10, 10, 10, 1.0)
-    print pe(pts)
+    x = simple_cubic_lattice(10, 10, 10, 1.0)
+    print f(x) 
+
+
